135f31e8a9eceea6bec980574010e4c0e7e178d7
Michal Ciebiada <michal.ciebiada@u2i.com>
2018-04-29 22:23:24 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const PouchDB = require('pouchdb')
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const pouchHandler = require('express-pouchdb')(PouchDB)
+
+const corsOptions = {
+  origin: process.env.WEB_HOST,
+  ccredentials: true
+}
+
+app.use('/', cors(corsOptions), pouchHandler) 
+
+app.listen(process.env.PORT || 5984)
+
+const users = new PouchDB('_users')
+
+users
+  .changes({
+    since: 'now',
+    live: true,
+    include_docs: true,
+    filter(doc) {
+      const version = doc._rev.split('-')[0]
+      if (parseInt(version) > 1 || doc.type !== 'user') { return false }
+      return true
+    }
+  })
+  .on('change', (result) => {
+    const username = result.doc.name
+    const userDB = new PouchDB(`userdb-${username}`)
+    userDB.putSecurity({
+      members: {
+        names: [username]
+      }
+    })
+  })
